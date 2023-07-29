791c22878323d29e0e0863dec6f2690379723157
david-sd96 <davidux.r00t@gmail.com>
2023-07-29 16:01:17 +0200

    RAX starts on background

--- RAX Installer.ps1
+++ RAX Installer.ps1
@@ -3,6 +3,6 @@ Copy-Item .\RAX $HOME -Recurse;
 
 #Generar tarea programada: ejecutar Launcher tras inicio de sesión con retardo
 $Trigger = New-ScheduledTaskTrigger -AtLogOn -RandomDelay (New-TimeSpan -Minutes 10);
-$Action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass -File $HOME\RAX\Launcher.ps1";
+$Action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-WindowStyle Hidden -NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass -File $HOME\RAX\Launcher.ps1";
 
 Register-ScheduledTask -TaskName "Remove-appx Auto eXecutable" -Trigger $Trigger -User $env:USERNAME -Action $Action -RunLevel Highest
