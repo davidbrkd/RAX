cbeb6d3a16ca1088dcae5a41bbdeec244eb9450e
david-sd96 <davidux.r00t@gmail.com>
2023-07-31 18:40:22 +0200

    banner added

--- RAX Installer.ps1
+++ RAX Installer.ps1
@@ -1,3 +1,16 @@
+Write-Host @"
+
+ooooooooo.    o.          'ooooo.   .ooo 
+'888  'Y88.   888.          '8888.  d8'  
+ 888  .d88'   8 888.          Y888.8b'   
+ 888ooo88P'   8  '888.         '8888.    
+ 888'88b.     88ooo8888.      .8PY888b.  
+ 888  '88b.   8'     '888.   .d8b  '888b.
+o888   o88o  o8o     o8888o o888    '888b.
+                                            
+Welcome to Remove Appx autoeXecutable!
+"@
+
 #Directorio de instalación: $HOME
 Copy-Item .\RAX $HOME -Recurse;
 
@@ -6,3 +19,6 @@ $Trigger = New-ScheduledTaskTrigger -AtLogOn -RandomDelay (New-TimeSpan -Minutes
 $Action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-WindowStyle Hidden -NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass -File $HOME\RAX\Launcher.ps1";
 
 Register-ScheduledTask -TaskName "Remove-appx Auto eXecutable" -Trigger $Trigger -User $env:USERNAME -Action $Action -RunLevel Highest
+
+Write-Host "RAX installed successfully!"
+Pause
