73f923994833d6718b9ed36a22d25528b2fb1a11
david-sd96 <davidux.r00t@gmail.com>
2023-10-24 23:05:19 +0200

    Update to launch CMD instead PS at scheduled task.

--- RAX Installer.ps1
+++ RAX Installer.ps1
@@ -16,7 +16,7 @@ Copy-Item .\RAX $HOME -Recurse;
 
 #Generar tarea programada: ejecutar Launcher tras inicio de sesión con retardo
 $Trigger = New-ScheduledTaskTrigger -AtLogOn -RandomDelay (New-TimeSpan -Minutes 10);
-$Action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-WindowStyle Hidden -NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass -File $HOME\RAX\Launcher.ps1";
+$Action = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/C START /B Powershell.exe -WindowStyle Hidden -NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass -File $HOME\RAX\Launcher.ps1";
 
 Register-ScheduledTask -TaskName "Remove-appx Auto eXecutable" -Trigger $Trigger -User $env:USERNAME -Action $Action -RunLevel Highest
 
